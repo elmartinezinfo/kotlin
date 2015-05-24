1844b66b60c4170b716ffbf7b9a19d193ae63938
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-05-24 22:41:55 +0300

    -- Count recursive call

--- idea/src/org/jetbrains/kotlin/idea/highlighter/KotlinRecursiveCallLineMarkerProvider.kt
+++ idea/src/org/jetbrains/kotlin/idea/highlighter/KotlinRecursiveCallLineMarkerProvider.kt
@@ -24,16 +24,27 @@ import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.SharedPsiElementImplUtil
+import com.intellij.psi.impl.source.resolve.reference.impl.PsiMultiReference
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.SimpleFunctionDescriptor
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.idea.formatter.JetBlock
+import org.jetbrains.kotlin.idea.kdoc.KDocReference
+import org.jetbrains.kotlin.idea.references.*
+import org.jetbrains.kotlin.kdoc.psi.impl.KDocName
+import org.jetbrains.kotlin.lexer.JetTokens
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.parents
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.inline.InlineUtil
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.resolve.scopes.receivers.ExpressionReceiver
 import org.jetbrains.kotlin.resolve.scopes.receivers.ReceiverValue
 import org.jetbrains.kotlin.resolve.scopes.receivers.ThisReceiver
+import org.jetbrains.kotlin.types.expressions.OperatorConventions
+import org.jetbrains.kotlin.utils.Profiler
 import java.util.HashSet
 
 public class KotlinRecursiveCallLineMarkerProvider() : LineMarkerProvider {
@@ -41,6 +52,12 @@ public class KotlinRecursiveCallLineMarkerProvider() : LineMarkerProvider {
 
     override fun collectSlowLineMarkers(elements: MutableList<PsiElement>, result: MutableCollection<LineMarkerInfo<*>>) {
         val markedLineNumbers = HashSet<Int>()
+
+        val profiler = Profiler.create("Recursive calls")
+        profiler.start()
+
+        profiler.printEntering()
+
         for (element in elements) {
             ProgressManager.checkCanceled()
             if (element is JetElement) {
@@ -51,22 +68,84 @@ public class KotlinRecursiveCallLineMarkerProvider() : LineMarkerProvider {
                 }
             }
         }
+
+        profiler.end()
     }
 
-    private fun getEnclosingFunction(element: JetElement): JetNamedFunction? {
+    private fun getEnclosingFunction(element: JetElement, stopOnNonInlinedLambdas: Boolean): JetNamedFunction? {
         for (parent in element.parents(false)) {
             when (parent) {
-                is JetFunctionLiteral -> if (!InlineUtil.isInlinedArgument(parent, parent.analyze(), false)) return null
-                is JetNamedFunction -> if (!InlineUtil.isInlinedArgument(parent, parent.analyze(), false)) return parent
+                is JetFunctionLiteral -> if (stopOnNonInlinedLambdas && !InlineUtil.isInlinedArgument(parent, parent.analyze(), false)) return null
+                is JetNamedFunction ->
+                    if (stopOnNonInlinedLambdas) {
+                        if (!InlineUtil.isInlinedArgument(parent, parent.analyze(), false)) return parent
+                    }
+                    else {
+                        when (parent.getParent()) {
+                            is JetBlockExpression -> return parent
+                            is JetClassBody -> return parent
+                            is JetFile -> return parent
+                            is JetScript -> return parent
+                        }
+                    }
                 is JetClassOrObject -> return null
             }
         }
         return null
     }
 
+    public fun <T: JetElement> AbstractJetReference<T>.expectedResolveName(): String? {
+        return when (this) {
+            is JetSimpleNameReference -> {
+                val element = getElement()
+                    val referenceText = element.getText()
+                    when (referenceText) {
+                        "==" -> "equals"
+                        "*" -> "times"
+                        "+" -> "plus"
+                        "-" -> "minus"
+                        "/" -> "div"
+                        "%" -> "mod"
+                        ".." -> "rangeTo"
+                        "++" -> "inc"
+                        "--" -> "dec"
+
+                        else -> referenceText
+                    }
+            }
+            is JetArrayAccessReference -> "get"
+            is JetForLoopInReference -> "iterator"
+            else -> null
+        }
+    }
+
     private fun isRecursiveCall(element: JetElement): Boolean {
-        if (element.getParent() is JetCallableReferenceExpression) return false
-        val enclosingFunction = getEnclosingFunction(element) ?: return false
+        val elementParent = element.getParent()
+        val resolveName = if (element !is JetArrayAccessExpression) {
+            when (elementParent) {
+                is JetCallExpression, is JetBinaryExpression,
+                is JetPostfixExpression, is JetPrefixExpression -> {
+                    val psiReference = element.findReferenceAt(0) as? AbstractJetReference<*> ?: return false
+                    if (psiReference !is JetSimpleNameReference) return false
+
+                    psiReference.expectedResolveName()
+                }
+
+                else -> null
+            }
+        }
+        else {
+            "get"
+        }
+
+        if (resolveName == null) return false
+
+        val enclosingFunction = getEnclosingFunction(element, false) ?: return false
+
+        if (enclosingFunction.getName() != resolveName) return false
+
+        // Check that there were no not-inlined lambda on the way to enclosing function
+        if (enclosingFunction != getEnclosingFunction(element, true)) return false
 
         val bindingContext = element.analyze()
         val enclosingFunctionDescriptor = bindingContext[BindingContext.FUNCTION, enclosingFunction] ?: return false
--- idea/testData/codeInsight/lineMarker/recursiveCall/conventionCall.kt
+++ idea/testData/codeInsight/lineMarker/recursiveCall/conventionCall.kt
@@ -3,3 +3,14 @@ fun Any.get(a: Int) {
         <lineMarker>this[a - 1]</lineMarker>
     }
 }
+
+class A {
+    override fun <lineMarker descr="Overrides function in 'Any'"></lineMarker>equals(other: Any?): Boolean {
+        this <lineMarker descr="Recursive call">==</lineMarker> other
+        return true
+    }
+
+    fun inc(): A {
+        return this<lineMarker descr="Recursive call">++</lineMarker>
+    }
+}
